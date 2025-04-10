9d99dd1f4e59512b28d8fce4b00c737aa561c735
Israel <israel.oliversw@gmail.com>
2025-04-10 09:56:56 -0300

    Faznedo o sprite se mover

--- Player.cs
+++ Player.cs
@@ -7,6 +7,8 @@ namespace Nyvorn;
 public class Player
 {
     public Rectangle rect;
+    float playerSpeed = 100f;
+
     Texture2D pixels;
 
     public Player(Texture2D texture)
@@ -14,7 +16,35 @@ public class Player
         rect = new Rectangle(100, 100, 272, 23);
         pixels = texture;
     }
-    public void Update(GameTime gameTime){}
+
+    public void Move(GameTime gameTime)
+    {
+        KeyboardState kState = Keyboard.GetState();
+        float updateSpeed = playerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+        if (kState.IsKeyDown(Keys.W))
+        {
+            rect.Y -= (int)updateSpeed; 
+        }
+        if (kState.IsKeyDown(Keys.S))
+        {
+            rect.Y += (int)updateSpeed;
+        }
+        if (kState.IsKeyDown(Keys.A))
+        {
+            rect.X -= (int)updateSpeed;
+        }
+        if (kState.IsKeyDown(Keys.D))
+        {
+            rect.X += (int)updateSpeed;
+        }
+
+    }
+    
+    public void Update(GameTime gameTime)
+    {
+        Move(gameTime);
+    }
     public void Draw(SpriteBatch spriteBatch)
     {
         spriteBatch.Draw(pixels, rect, Color.White); 
