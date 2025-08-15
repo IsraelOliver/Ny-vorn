971fcd2e58a2588377c8a553cadfec45119aedd1
Israel <israel.oliversw@gmail.com>
2025-08-15 18:01:16 -0300

    Finalmente a colisão foi arrumada.

--- obj/Debug/net8.0/Nyvorn.AssemblyInfo.cs
+++ obj/Debug/net8.0/Nyvorn.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Nyvorn")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7102c2858dfc96da5b6a67f0012b5ff1f92ede9d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b81b67e10ef879f35c90c2c779138df263eb5a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Nyvorn")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Nyvorn")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
